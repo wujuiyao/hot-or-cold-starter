607cbf658865be6b07d8cbe22ab3ea364cf27fe5
Richard Wu <contactrichardwu@gmail.com>
2015-05-20 21:55:55 +0800

    just comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +12,16 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	/******New Game Button Function******/
+	function newGame(){}
+	/*Game starts with random number*/
+	/*make sure the input are numeric*/
+	/*user guess and determines which feedback to provide*/
+	/*Feedback about the guess should appear in div#feedback*/
+	/*record all the guesses + all the numbers they have guessed*/
+
+
+
+
+
 });
-
-
