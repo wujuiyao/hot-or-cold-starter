083df75516ef2060f54242bae0a3e3552b7cced5
Richard Wu <contactrichardwu@gmail.com>
2015-05-21 19:28:39 +0800

    Make sure it is numeric number

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
   	});
 
-  	/*--- Hide information modal box ---*/
+  /*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
@@ -15,13 +15,27 @@ $(document).ready(function(){
 	/******New Game Button Function******/
 	function newGame(){}
 	/*Game starts with random number*/
+	function randomNumber(){}
 	/*make sure the input are numeric*/
+	function userNumber(){
+		var guessButton = document.getElementById('guessButton');
+		guessButton.addEventListener('click', function(){
+			var userGuess = document.getElementById('userGuess').value;
+			if(userGuess > 100 || userGuess < 0){
+					alert("Number between 0 and 100");
+			}else if(isNaN(userGuess)){
+					alert("Numeric Number Please");
+			}
+		}, false);
+
+	}
+
 	/*user guess and determines which feedback to provide*/
 	/*Feedback about the guess should appear in div#feedback*/
 	/*record all the guesses + all the numbers they have guessed*/
 
+	/*how to start writing functions*/
 
-
-
+	var uN = new userNumber();
 
 });
