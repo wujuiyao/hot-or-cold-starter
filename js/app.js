98bc053929c7e90b00791dbf3f7fcceb3f42afc3
Richard Wu <contactrichardwu@gmail.com>
2015-05-27 21:24:46 +0800

    Test

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
 
 $(document).ready(function(){
 
-
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
-
   /*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+	var game = new createRandomNumber();
 
-	/******New Game Button Function******/
-	function newGame(){}
-	/*Game starts with random number*/
-	function randomNumber(){
-		
+	var userNum = new userNumber();
+
+	function createRandomNumber(){
+
+
+    function getRandomInt(){
+			return Math.floor(Math.random()*100) + 1;
+		}
+		function randomNumbers(){
+			var num;
+			num = getRandomInt();
+			return num;
+		}
+		var rN = randomNumbers();
+		console.log(rN);
+
 	}
 	/*make sure the input are numeric*/
 	function userNumber(){
@@ -30,14 +40,44 @@ $(document).ready(function(){
 					alert('Numeric Number Please');
 			}
 		}, false);
+		/*user guess and determines which feedback to provide*/
+		/*Feedback about the guess should appear in div#feedback*/
 	}
-
-	/*user guess and determines which feedback to provide*/
-	/*Feedback about the guess should appear in div#feedback*/
 	/*record all the guesses + all the numbers they have guessed*/
-
 	/*how to start writing functions*/
-
-	var uN = new userNumber();
-
 });
+
+
+// function findSolution(target) {
+//   function find(start, history) {
+//     if (start == target)
+//       return history;
+//     else if (start > target)
+//       return null;
+//     else
+//       return find(start + 5, "(" + history + " + 5)") ||
+//              find(start * 3, "(" + history + " * 3)");
+//   }
+//   return find(1, "1");
+// }
+//
+// console.log(findSolution(24));
+// // → (((1 * 3) + 5) * 3)
+
+// function zeroPad(number, width) {
+//   var string = String(number);
+//   while (string.length < width)
+//     string = "0" + string;
+//   return string;
+// }
+//
+// function printFarmInventory(cows, chickens, pigs) {
+//   console.log(zeroPad(cows, 3) + " Cows");
+//   console.log(zeroPad(chickens, 3) + " Chickens");
+//   console.log(zeroPad(pigs, 3) + " Pigs");
+// }
+//
+// printFarmInventory(7, 16, 3);
+
+
+// array of the random numbers, and use for loop inside the array
