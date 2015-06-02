afe421a566be2972c18848635dcb490d7f7c18eb
Richard Wu <contactrichardwu@gmail.com>
2015-06-02 21:54:43 +0800

    remove comments, clean up lines

--- js/app.js
+++ js/app.js
@@ -35,6 +35,8 @@ $(document).ready(function(){
        var guessButton = document.getElementById('guessButton');
        /*Number guessed by User*/
        var userGuess;
+       /*Finish or start again*/
+       var finish = false;
 
        guessButton.addEventListener('click', function (){
           userGuess = document.getElementById('userGuess').value;
@@ -100,44 +102,7 @@ $(document).ready(function(){
           guesses ++;
           document.getElementById('count').innerHTML = guesses;
           console.log(guesses);
+      //
        }, false);
      }
-		/*user guess and determines which feedback to provide*/
-		/*Feedback about the guess should appear in div#feedback*/
 });
-
-
-
-// function findSolution(target) {
-//   function find(start, history) {
-//     if (start == target)
-//       return history;
-//     else if (start > target)
-//       return null;
-//     else
-//       return find(start + 5, "(" + history + " + 5)") ||
-//              find(start * 3, "(" + history + " * 3)");
-//   }
-//   return find(1, "1");
-// }
-//
-// console.log(findSolution(24));
-// // → (((1 * 3) + 5) * 3)
-
-// function zeroPad(number, width) {
-//   var string = String(number);
-//   while (string.length < width)
-//     string = "0" + string;
-//   return string;
-// }
-//
-// function printFarmInventory(cows, chickens, pigs) {
-//   console.log(zeroPad(cows, 3) + " Cows");
-//   console.log(zeroPad(chickens, 3) + " Chickens");
-//   console.log(zeroPad(pigs, 3) + " Pigs");
-// }
-//
-// printFarmInventory(7, 16, 3);
-
-
-// array of the random numbers, and use for loop inside the array
