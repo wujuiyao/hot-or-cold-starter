28899ac76dd33d0a82a23adeaa76f4e2ec0dc58b
Richard Wu <contactrichardwu@gmail.com>
2015-06-11 22:49:31 +0800

    Try Reset Game

--- js/app.js
+++ js/app.js
@@ -18,12 +18,40 @@ $(document).ready(function(){
     var distance;
     var finishState = false;
 
-    /*Generate a Secret Number*/
     function randomInt() {
         secretNumber = Math.floor(Math.random()*100) + 1;
         console.log("Secret Number is " + secretNumber);
     }
     randomInt();
+
+    function compareNumber(){
+      switch(true){
+        case userGuess / secretNumber == 1:
+          feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
+          finishState = true;break;
+        case distance > 60:
+          feedback.innerHTML = "Its extreme freezing"; break;
+        case distance > 50:
+          feedback.innerHTML = "You are freezing"; break;
+        case distance > 40:
+          feedback.innerHTML = "Its very cold"; break;
+        case distance > 30:
+          feedback.innerHTML = "Its cold"; break;
+        case distance > 20:
+          feedback.innerHTML = "It's getting warm"; break;
+        case distance > 10:
+          feedback.innerHTML = "It's getting very warm"; break;
+        case distance > 5:
+          feedback.innerHTML = "It's hot"; break;
+        case distance > 3.5:
+          feedback.innerHTML = "It's very hot"; break;
+        case distance > 1.5:
+          feedback.innerHTML = "It's super hot"; break;
+        case distance > 0.5:
+          feedback.innerHTML = "It's burning!"; break;
+        default: break;
+      }
+    }
 
     /*Evaluates the Input*/
     function checkInput(){
@@ -38,32 +66,7 @@ $(document).ready(function(){
          feedback.innerHTML = 'Enter a number from 1 - 100';
          clickCount = 0-1;
        }else if (1 < userGuess < 101){
-         switch(true){
-           case userGuess / secretNumber == 1:
-             feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
-             finishState = true;break;
-           case distance > 60:
-             feedback.innerHTML = "Its extreme freezing"; break;
-           case distance > 50:
-             feedback.innerHTML = "You are freezing"; break;
-           case distance > 40:
-             feedback.innerHTML = "Its very cold"; break;
-           case distance > 30:
-             feedback.innerHTML = "Its cold"; break;
-           case distance > 20:
-             feedback.innerHTML = "It's getting warm"; break;
-           case distance > 10:
-             feedback.innerHTML = "It's getting very warm"; break;
-           case distance > 5:
-             feedback.innerHTML = "It's hot"; break;
-           case distance > 3.5:
-             feedback.innerHTML = "It's very hot"; break;
-           case distance > 1.5:
-             feedback.innerHTML = "It's super hot"; break;
-           case distance > 0.5:
-             feedback.innerHTML = "It's burning!"; break;
-             default: break;
-         }
+         compareNumber();
       }
     }
 
@@ -97,9 +100,9 @@ $(document).ready(function(){
       finishState = false;
       secretNumber = randomInt();
       initFeedback();
-
+      
       clickCount = 0;
-      window.userGuess = 0;
+      userGuess = 0;
     }
 
     /*New Game*/
@@ -111,4 +114,5 @@ $(document).ready(function(){
       });
      }
     startNewGame();
+
 });
