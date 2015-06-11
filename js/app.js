493c70c58b2d3c346d38610fbb51de164a7737a9
Richard Wu <contactrichardwu@gmail.com>
2015-06-11 22:18:40 +0800

    change to Switch

--- js/app.js
+++ js/app.js
@@ -30,37 +30,41 @@ $(document).ready(function(){
       var feedback = document.getElementById('feedback');
       userGuess = document.getElementById('userGuess').value;
       distance = Math.abs(secretNumber - userGuess);
-        if(userGuess > 100 || userGuess < 0){
-           feedback.innerHTML = 'Number between 0 and 100!';
-         }else if(isNaN(userGuess)){
-           feedback.innerHTML = 'Numeric Number Please!';
-         }else if(userGuess === ""){
-           feedback.innerHTML = 'Enter a number from 1 - 100';
-           clickCount = 0-1;
-         }else if (1 < userGuess < 101){
-           if(userGuess / secretNumber == 1){
-              feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
-              finishState = true;
-            }else if(distance > 50){
-              feedback.innerHTML = "You are freezing";
-            }else if(distance > 40){
-              feedback.innerHTML = 'Its very cold';
-            }else if(distance > 30){
-              feedback.innerHTML = "Its cold";
-            }else if(distance > 20){
-              feedback.innterHTML = "It's getting warm";
-            }else if(distance > 10){
-              feedback.innerHTML = "It's getting very warm";
-            }else if(distance > 5){
-              feedback.innerHTML = "It's hot";
-            }else if(distance > 3.5){
-              feedback.innerHTML = "It's very hot";
-            }else if(distance > 1.5){
-              feedback.innerHTML = "It's super hot";
-            }else if(distance > 0.5){
-              feedback.innerHTML = "It's Burning!!!";
-            }else{}
-        }
+      if(userGuess > 100 || userGuess < 0){
+         feedback.innerHTML = 'Number between 0 and 100!';
+       }else if(isNaN(userGuess)){
+         feedback.innerHTML = 'Numeric Number Please!';
+       }else if(userGuess === ""){
+         feedback.innerHTML = 'Enter a number from 1 - 100';
+         clickCount = 0-1;
+       }else if (1 < userGuess < 101){
+         switch(true){
+           case userGuess / secretNumber == 1:
+             feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
+             finishState = true;break;
+           case distance > 60:
+             feedback.innerHTML = "Its extreme freezing"; break;
+           case distance > 50:
+             feedback.innerHTML = "You are freezing"; break;
+           case distance > 40:
+             feedback.innerHTML = "Its very cold"; break;
+           case distance > 30:
+             feedback.innerHTML = "Its cold"; break;
+           case distance > 20:
+             feedback.innerHTML = "It's getting warm"; break;
+           case distance > 10:
+             feedback.innerHTML = "It's getting very warm"; break;
+           case distance > 5:
+             feedback.innerHTML = "It's hot"; break;
+           case distance > 3.5:
+             feedback.innerHTML = "It's very hot"; break;
+           case distance > 1.5:
+             feedback.innerHTML = "It's super hot"; break;
+           case distance > 0.5:
+             feedback.innerHTML = "It's burning!"; break;
+             default: break;
+         }
+      }
     }
 
     /*Count the Clicks function*/
@@ -83,7 +87,6 @@ $(document).ready(function(){
     }
     clickGuess();
 
-
     function initFeedback(){
       document.getElementById('feedback').innerHTML = "Make your Guess!";
     }
@@ -95,7 +98,7 @@ $(document).ready(function(){
       secretNumber = randomInt();
       initFeedback();
 
-      window.clickCount = 0;
+      clickCount = 0;
       window.userGuess = 0;
     }
 
@@ -104,9 +107,8 @@ $(document).ready(function(){
       var newGame = document.getElementById("new");
       newGame.addEventListener('click', function(){
         reset();
+        return false;
       });
      }
     startNewGame();
-
-
 });
