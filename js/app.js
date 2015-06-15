8cd37631e2258ca9a8c6287fca8733448b73f560
Richard Wu <contactrichardwu@gmail.com>
2015-06-16 00:02:06 +0800

    re strucutre

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function(){
+  /*State of the App*/
+  var secretNumber = '';
+  var userGuess = '';
+  var clickCount = 0;
+  var finishState = false;
+  var feedback = document.getElementById('feedback');
+  var guessButton = document.getElementById('guessButton');
+  var myClicks = document.getElementById('count');
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,101 +19,79 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-    /**************************************************/
-    var secretNumber = 0;
-    var userGuess = 0;
-    var clickCount = 0;
-    var distance;
-    var finishState = false;
+  /*Processes click on guess button */
+  guessButton.addEventListener('click', function(){
+    clickCount++;
+    myClicks.innerHTML = clickCount;
+    if(finishState === false){
+      checkInput();
+      addNumber();
+    } else {
+      feedback.innerHTML = "You already won! Start a New Game";
+    }
+  });
+  /*Processes click on new button*/
+  document.getElementById('new').addEventListener('click');
+
+  /* Utility Functions */
 
     function randomInt() {
-        secretNumber = Math.floor(Math.random()*100) + 1;
-        console.log("Secret Number is " + secretNumber);
+        var randomNumber = Math.floor(Math.random()*100) + 1;
+        return randomNumber;
     }
-    randomInt();
-
-
-    function clickGuess(){
-      var feedback = document.getElementById('feedback');
-      var guessButton = document.getElementById('guessButton');
-      var myClicks = document.getElementById('count');
-
-      guessButton.addEventListener('click', function(){
-        clickCount++;
-        myClicks.innerHTML = clickCount;
-        if(finishState === false){
-          checkInput();
-          addNode();
-        } else {
-          feedback.innerHTML = "You already won! Start a New Game";
-        }
-        return false;
-      });
-    }
-    clickGuess();
+    secretNumber = randomInt();
+    console.log("Secret Number is " + secretNumber);
 
     function checkInput(){
       userGuess = document.getElementById('userGuess').value;
-      distance = Math.abs(secretNumber - userGuess);
       if(userGuess > 100 || userGuess < 0){
-         feedback.innerHTML = 'Number between 0 and 100!';
+        feedback.innerHTML = 'Number between 0 and 100!';
        }else if(isNaN(userGuess)){
          feedback.innerHTML = 'Numeric Number Please!';
        }else if(userGuess === ""){
          feedback.innerHTML = 'Enter a number from 1 - 100';
        }else if (1 < userGuess < 101){
-         compareNumber();
+         var distance = Math.abs(secretNumber - userGuess);
+         switch(true){
+           case userGuess / secretNumber == 1:
+             feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
+             finishState = true;break;
+           case distance > 60:
+             feedback.innerHTML = "Its extreme freezing"; break;
+           case distance > 50:
+             feedback.innerHTML = "You are freezing"; break;
+           case distance > 40:
+             feedback.innerHTML = "Its very cold"; break;
+           case distance > 30:
+             feedback.innerHTML = "Its cold"; break;
+           case distance > 20:
+             feedback.innerHTML = "It's getting warm"; break;
+           case distance > 10:
+             feedback.innerHTML = "It's getting very warm"; break;
+           case distance > 5:
+             feedback.innerHTML = "It's hot"; break;
+           case distance > 3.5:
+             feedback.innerHTML = "It's very hot"; break;
+           case distance > 1.5:
+             feedback.innerHTML = "It's super hot"; break;
+           case distance > 0.5:
+             feedback.innerHTML = "It's burning!"; break;
+           default: break;
+         }
       }
     }
 
-    function compareNumber(){
-      switch(true){
-        case userGuess / secretNumber == 1:
-          feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
-          finishState = true;break;
-        case distance > 60:
-          feedback.innerHTML = "Its extreme freezing"; break;
-        case distance > 50:
-          feedback.innerHTML = "You are freezing"; break;
-        case distance > 40:
-          feedback.innerHTML = "Its very cold"; break;
-        case distance > 30:
-          feedback.innerHTML = "Its cold"; break;
-        case distance > 20:
-          feedback.innerHTML = "It's getting warm"; break;
-        case distance > 10:
-          feedback.innerHTML = "It's getting very warm"; break;
-        case distance > 5:
-          feedback.innerHTML = "It's hot"; break;
-        case distance > 3.5:
-          feedback.innerHTML = "It's very hot"; break;
-        case distance > 1.5:
-          feedback.innerHTML = "It's super hot"; break;
-        case distance > 0.5:
-          feedback.innerHTML = "It's burning!"; break;
-        default: break;
-      }
-    }
-
-
-    function initFeedback(){
-      document.getElementById('feedback').innerHTML = "Make your Guess!";
-    }
-
-    /*reset game details*/
     function reset(){
-      console.log("Starting a new Game");
+      console.log("Start a new Game");
       finishState = false;
-      secretNumber = randomInt();
-      initFeedback();
-
-      clickCount = 0;
-      userGuess = 0;
+      userGuess.innerHTML = '';
+      clickCount = '';
+      randomInt();
+      document.getElementById('feedback').innerHTML = "Make your Guess!";
+      document.getElementById('guessList').innerHTML = '';
     }
 
-    document.getElementById('new').addEventListener('click', removeNode, false);
-
-    function addNode(){
+    function addNumber(){
       var inText = document.getElementById('userGuess').value;
       var newText = document.createTextNode(inText);
       var newLi = document.createElement("li");
@@ -115,24 +101,11 @@ $(document).ready(function(){
       list.appendChild(newLi);
     }
 
-    function removeNode(){
-      var allLi = document.getElementsByTagName('li');
-      if(allLi.length > 1){
-        var list = document.getElementById('guessList');
-        list.removeChild(allLi);
-      }else{
-        alert('Nothing to Delete');
-      }
-    }
 
-    function book(){
-      var allGrafs = document.getElementsByTagName("p");
-      if (allGrafs.length > 1){
-        var lastGraf = allGrafs[all.Grafs.length-1];
-        var docBody = document.getElementsByTagName("body")[0];
-        docbody.removeChild(lastGraf);
-      }
+    function removeNumber(){
+      var lists = document.getElementsByTagName("li");
+      var list = document.getElementById('guessList');
+      list.removeChild(lists);
     }
-
 
 });
