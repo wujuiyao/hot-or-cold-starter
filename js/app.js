6e44d872778c666d5aea0180a623f6d915e2fc0e
Richard Wu <contactrichardwu@gmail.com>
2015-06-17 00:17:06 +0800

    Clean up code + reset game

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 
 $(document).ready(function(){
   /*State of the App*/
-  var secretNumber = '';
-  var userGuess = '';
+  var secretNumber;
+  var userGuess;
   var clickCount = 0;
   var finishState = false;
   var feedback = document.getElementById('feedback');
   var guessButton = document.getElementById('guessButton');
   var myClicks = document.getElementById('count');
 
+  /*Random Number*/
+  secretNumber = Math.floor(Math.random()*100) + 1;
+  console.log("Secret Number is " + secretNumber);
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
+
   /*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
@@ -27,20 +32,16 @@ $(document).ready(function(){
       checkInput();
       addNumber();
     } else {
-      feedback.innerHTML = "You already won! Start a New Game";
+      feedback.innerHTML = "You already won! Start New Game";
+      clickCount = clickCount - 1;
+      myClicks.innerHTML = clickCount;
     }
   });
+
   /*Processes click on new button*/
-  document.getElementById('new').addEventListener('click');
+  document.getElementById('new').addEventListener('click', reset);
 
-  /* Utility Functions */
-
-    function randomInt() {
-        var randomNumber = Math.floor(Math.random()*100) + 1;
-        return randomNumber;
-    }
-    secretNumber = randomInt();
-    console.log("Secret Number is " + secretNumber);
+  /*Utility Functions*/
 
     function checkInput(){
       userGuess = document.getElementById('userGuess').value;
@@ -52,43 +53,31 @@ $(document).ready(function(){
          feedback.innerHTML = 'Enter a number from 1 - 100';
        }else if (1 < userGuess < 101){
          var distance = Math.abs(secretNumber - userGuess);
-         switch(true){
-           case userGuess / secretNumber == 1:
-             feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
-             finishState = true;break;
-           case distance > 60:
-             feedback.innerHTML = "Its extreme freezing"; break;
-           case distance > 50:
-             feedback.innerHTML = "You are freezing"; break;
-           case distance > 40:
-             feedback.innerHTML = "Its very cold"; break;
-           case distance > 30:
-             feedback.innerHTML = "Its cold"; break;
-           case distance > 20:
-             feedback.innerHTML = "It's getting warm"; break;
-           case distance > 10:
-             feedback.innerHTML = "It's getting very warm"; break;
-           case distance > 5:
-             feedback.innerHTML = "It's hot"; break;
-           case distance > 3.5:
-             feedback.innerHTML = "It's very hot"; break;
-           case distance > 1.5:
-             feedback.innerHTML = "It's super hot"; break;
-           case distance > 0.5:
-             feedback.innerHTML = "It's burning!"; break;
-           default: break;
-         }
+         if(userGuess / secretNumber == 1){
+           feedback.innerHTML = "Great, " + userGuess + " is the correct number !";
+           finishState = true;
+         }else if(distance > 60){
+           feedback.innerHTML = "Its Extreme Freezing";
+         }else if(distance > 50){
+           feedback.innerHTML = "Its Freezing";
+         }else if(distance > 40){
+           feedback.innerHTML = "Its very cold";
+         }else if(distance > 30){
+           feedback.innerHTML = "Its cold";
+         }else if(distance > 20){
+           feedback.innerHTML = "Its getting Warm";
+         }else if(distance > 10){
+           feedback.innerHTML = "Its getting very warm";
+         }else if(distance > 5.5){
+           feedback.innerHTML = "Its hot!";
+         }else if(distance > 3.5){
+           feedback.innerHTML = "Its very hot!";
+         }else if(distance > 1.5){
+           feedback.innerHTML = "Its super hot";
+         }else if(distance > 0.5){
+           feedback.innerHTML = "Its burning";
+         }else{}
       }
-    }
-
-    function reset(){
-      console.log("Start a new Game");
-      finishState = false;
-      userGuess.innerHTML = '';
-      clickCount = '';
-      randomInt();
-      document.getElementById('feedback').innerHTML = "Make your Guess!";
-      document.getElementById('guessList').innerHTML = '';
     }
 
     function addNumber(){
@@ -96,16 +85,26 @@ $(document).ready(function(){
       var newText = document.createTextNode(inText);
       var newLi = document.createElement("li");
       newLi.appendChild(newText);
-
       var list = document.getElementById('guessList');
       list.appendChild(newLi);
     }
 
+    function reset(){
+      console.log("Start a new Game");
 
-    function removeNumber(){
-      var lists = document.getElementsByTagName("li");
-      var list = document.getElementById('guessList');
-      list.removeChild(lists);
+      finishState = false;
+
+      userGuess = document.getElementById('userGuess');
+      userGuess.value = '';
+
+      clickCount = 0;
+      myClicks.innerHTML = clickCount;
+
+      document.getElementById('feedback').innerHTML = "Make your Guess!";
+      document.getElementById('guessList').innerHTML = '';
+
+      secretNumber =  Math.floor(Math.random()*100) + 1;
+      console.log('New Secret Number is ' + secretNumber);
     }
 
 });
